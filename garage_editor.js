f1df364ad21c727e932d0a7abbac1793ece90b4b
Ray Adams <63924749+Ray-Adams@users.noreply.github.com>
2021-03-04 15:13:22 -0800

    Minor refactor

--- garage_editor.js
+++ garage_editor.js
@@ -1,20 +1,32 @@
 (async () => {
-    let garageSections = prompt('Enter the number of garage sections you would like to have in total (NOTE: There are 30 individual garage slots per section):');
-    let uhashParam = `uhash=${encodeURI(document.cookie.match('(^|;) *ntuserrem=([^;]*)')[2])}`, bodyContent = '';
+
+    const garageSections = prompt('Enter the number of garage sections you would like to have in total (NOTE: There are 30 individual garage slots per section):');
+    
+    /* Source: https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie */
+    const getCookieValue = name => {
+        return document.cookie
+            .split('; ')
+            .find(element => element.startsWith(name + '='))
+            .split('=')[1];
+    };
+    
+    const uhashParameter = 'uhash=' + getCookieValue('ntuserrem');
   
+    let i, garageParameter = '';
+
     for (i = 0; i < garageSections * 30; i++) {
-      bodyContent += 'garage%5B' + i + '%5D=&';
-    }
-    bodyContent += uhashParam;
+        garageParameter += 'garage%5B' + i + '%5D=&';
+    };
+  
   
     await fetch("https://www.nitrotype.com/api/cars-arrange", {
       "headers": {
         "Content-Type": "application/x-www-form-urlencoded"
       },
-      "body": bodyContent,
+      "body": garageParameter + uhashParameter,
       "method": "POST",
       "mode": "cors"
-    })
+    });
     
     alert("Logging you out... Please log back in to see changes.");
 
@@ -22,10 +34,11 @@
       "headers": {
         "Content-Type": "application/x-www-form-urlencoded"
       },
-      "body": uhashParam,
+      "body": uhashParameter,
       "method": "POST",
       "mode": "cors"
-    })
+    });
 
     location.reload();
-  })()
+
+})()
